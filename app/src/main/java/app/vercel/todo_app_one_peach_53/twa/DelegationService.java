f199151422cd4369369729b66aef52203b10d0a6
maok026 <maokawai.asanoha@gmail.com>
2026-03-20 20:02:44 +0900

    assetlinks.jsonとvercel.jsonを追加

---
+++ app/src/main/java/app/vercel/todo_app_one_peach_53/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.todo_app_one_peach_53.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
